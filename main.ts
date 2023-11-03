107d2082fcf809c0b996afcedd728968f538f610
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2023-11-03 08:24:14 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,41 +3,34 @@ enum RadioMessage {
     Test = 2239
 }
 radio.onReceivedNumber(function (receivedNumber) {
-    if (radio.receivedPacket(RadioPacketProperty.SerialNumber) == parseFloat("-654391920")) {
-        basic.showString("L")
-        if (radio.receivedPacket(RadioPacketProperty.SerialNumber) == parseFloat("1750929838")) {
-            basic.showString("M")
-        }
-    }
     basic.showNumber(receivedNumber)
 })
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     radio.sendNumber(1)
+    basic.showNumber(1)
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     radio.sendString("A")
+    basic.showString("A")
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     radio.sendString("B")
+    basic.showString("B")
 })
 radio.onReceivedString(function (receivedString) {
-    if (radio.receivedPacket(RadioPacketProperty.SerialNumber) == parseFloat("-654391920")) {
-        basic.showString("L")
-        if (radio.receivedPacket(RadioPacketProperty.SerialNumber) == parseFloat("1750929838")) {
-            basic.showString("M")
-        }
-    }
     basic.showString(receivedString)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
     radio.sendNumber(0)
+    basic.showString("B")
 })
 input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
     radio.sendNumber(2)
+    basic.showNumber(2)
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
     radio.sendNumber(3)
+    basic.showNumber(3)
 })
-radio.setTransmitSerialNumber(true)
-radio.setGroup(4)
+radio.setGroup(12)
 radio.setTransmitPower(7)
